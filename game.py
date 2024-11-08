0d7b09d368b522a74a0b353b7f08ff6272714ffb
meganclapinski25 <142531799+meganclapinski25@users.noreply.github.com>
2024-11-07 17:16:36 -0800

    Created start of pygame (1)

---
+++ game.py
@@ -0,0 +1,24 @@
+# Import and initialize pygame
+import pygame 
+pygame.init()
+
+# Configure the screen
+screen = pygame.display.set_mode([500, 500])
+
+# game loop 
+running = True 
+while running: 
+        # Looking at the events 
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+                
+        # Clear the screen 
+        screen.fill((255,255,255))
+        
+        #Circle
+        color = (255, 0, 255)
+        positon = (250,250)
+        pygame.draw.cirlce(screen, color, postion, 75)
+        # Update
+        pygame.display.flip()
