f9fdec367e1c25654ccdf9aa963902888b520423
meganclapinski25 <142531799+meganclapinski25@users.noreply.github.com>
2024-11-08 10:20:10 -0800

    Strawberry and Apple moving

--- game.py
+++ game.py
@@ -1,10 +1,12 @@
 # Import and initialize pygame
 import pygame 
+from random import randint
 pygame.init()
 
 # Configure the screen
 screen = pygame.display.set_mode([500, 500])
-
+# Get the clock
+clock = pygame.time.Clock()
 
 class GameObject(pygame.sprite.Sprite):
     def __init__(self, x, y, image):
@@ -19,15 +21,53 @@ class GameObject(pygame.sprite.Sprite):
         # screen.fill((63,63,63))
 
 apple = GameObject(100, 400, 'apple.png') 
-apple2 = GameObject(100, 100, 'apple.png') 
-apple3 = GameObject(250, 250, 'apple.png') 
-apple4 = GameObject(400, 100, 'apple.png') 
-apple5 = GameObject(400, 400, 'apple.png') 
 
-strawberry = GameObject(100, 250, 'strawberry.png')
-strawberry1 = GameObject(400,250, 'strawberry.png')
-strawberry2 = GameObject(250,100, 'strawberry.png')
-strawberry3 = GameObject(250,400, 'strawberry.png')
+class Apple(GameObject):
+ def __init__(self):
+   super(Apple, self).__init__(0, 0, 'apple.png')
+   self.dx = 0
+   self.dy = (randint(0, 200) / 100) + 1
+   self.reset() # call reset here! 
+
+ def move(self):
+   self.x += self.dx
+   self.y += self.dy
+   # Check the y position of the apple
+   if self.y > 500: 
+     self.reset()
+
+ # add a new method
+ def reset(self):
+   self.x = randint(50, 400)
+   self.y = -64
+
+class Strawberry(GameObject):
+    def __init__(self):
+        super(Strawberry, self).__init__(0,0, 'strawberry.png')
+        self.dx = 0
+        self.dy = (randint(0, 200) / 100) + 1
+        self.reset() # call reset here!
+    def move(self):
+        self.x += self.dx
+        self.y += self.dy
+        if self.y > 500: 
+            self.reset()
+    def reset(self):
+        self.x = randint(50, 400)
+        self.y = -64
+
+
+apple = Apple() 
+strawberry = Strawberry()
+# apple2 = GameObject(100, 100, 'apple.png') 
+# apple3 = GameObject(250, 250, 'apple.png') 
+# apple4 = GameObject(400, 100, 'apple.png') 
+# apple5 = GameObject(400, 400, 'apple.png') 
+
+# strawberry = GameObject(100, 250, 'strawberry.png')
+# strawberry1 = GameObject(400,250, 'strawberry.png')
+# strawberry2 = GameObject(250,100, 'strawberry.png')
+# strawberry3 = GameObject(250,400, 'strawberry.png')
 
 # game loop 
 running = True 
@@ -40,16 +80,22 @@ while running:
         # Clear the screen 
         screen.fill((255,255,255))
         # ADD!
+        apple.x += 1.5
+        apple.move()
         apple.render(screen)
-        apple2.render(screen)
-        apple3.render(screen)
-        apple4.render(screen)
-        apple5.render(screen)
+        strawberry.x +=1.5
+        strawberry.move()
         strawberry.render(screen)
-        strawberry1.render(screen)
-        strawberry2.render(screen)
-        strawberry3.render(screen)
+        # apple2.render(screen)
+        # apple3.render(screen)
+        # apple4.render(screen)
+        # apple5.render(screen)
+        # strawberry.render(screen)
+        # strawberry1.render(screen)
+        # strawberry2.render(screen)
+        # strawberry3.render(screen)
         pygame.display.flip()
+        clock.tick(60)
         
 
         
