33a9398fcbb5e044072c09ebeab561227ad01a78
meganclapinski25 <142531799+meganclapinski25@users.noreply.github.com>
2024-11-07 21:26:14 -0800

    Start Challenge 2

--- game.py
+++ game.py
@@ -5,6 +5,20 @@ pygame.init()
 # Configure the screen
 screen = pygame.display.set_mode([500, 500])
 
+
+class GameObject(pygame.sprite.Sprite):
+    def __init__(self, x, y, image):
+        super(GameObject, self).__init__()
+        self.surf = pygame.image.load(image)
+        self.x = x
+        self.y = y
+
+    def render(self, screen):
+        screen.blit(self.surf, (self.x, self.y))
+        # Challenge 1 
+        # screen.fill((63,63,63))
+apple = GameObject(120, 300, 'apple.png') 
+
 # game loop 
 running = True 
 while running: 
@@ -14,18 +28,15 @@ while running:
                 running = False
                 
         # Clear the screen 
-        # screen.fill((255,255,255))
+        screen.fill((255,255,255))
+        apple.render(screen) # ADD!
+        pygame.display.flip()
         
-        # #Circle
-        # color = (25, 184, 22)
-        # positon = (250,250)
-        # pygame.draw.circle(screen, color, positon, 75)
-        # # Update
-        # pygame.display.flip()
+
         
         
-        # Challenge 1 
-        screen.fill((63,63,63))
+       
+    # Make an instance of GameObject
         
         # # Circle 1 
         # color = (0,255,0)
@@ -56,49 +67,49 @@ while running:
         #Challenge 2 
         
         # Circle 1 
-        color = (169,169,169)
-        postion = (100, 400)
-        pygame.draw.circle(screen, color, postion, 55)
+        # color = (169,169,169)
+        # postion = (100, 400)
+        # pygame.draw.circle(screen, color, postion, 55)
        
-        #Circle 2 
-        color = (169,169,169)
-        postion = (100, 100)
-        pygame.draw.circle(screen, color, postion, 55)
+        # #Circle 2 
+        # color = (169,169,169)
+        # postion = (100, 100)
+        # pygame.draw.circle(screen, color, postion, 55)
         
-        #Circle 3
-        color = (169,169,169)
-        postion = (250, 250)
-        pygame.draw.circle(screen, color, postion, 55)
+        # #Circle 3
+        # color = (169,169,169)
+        # postion = (250, 250)
+        # pygame.draw.circle(screen, color, postion, 55)
         
-        #Circle 4
-        color = (169,169,169)
-        postion = (400, 100)
-        pygame.draw.circle(screen, color, postion, 55)
+        # #Circle 4
+        # color = (169,169,169)
+        # postion = (400, 100)
+        # pygame.draw.circle(screen, color, postion, 55)
         
-        #Circle 5
-        color = (169,169,169)
-        postion = (400, 400)
-        pygame.draw.circle(screen, color, postion, 55)
+        # #Circle 5
+        # color = (169,169,169)
+        # postion = (400, 400)
+        # pygame.draw.circle(screen, color, postion, 55)
        
-        # Circle 6 
-        color = (169,169,169)
-        postion = (100, 250)
-        pygame.draw.circle(screen, color, postion, 55)
+        # # Circle 6 
+        # color = (169,169,169)
+        # postion = (100, 250)
+        # pygame.draw.circle(screen, color, postion, 55)
        
-        #Circle 7
-        color = (169,169,169)
-        postion = (400, 250)
-        pygame.draw.circle(screen, color, postion, 55)
+        # #Circle 7
+        # color = (169,169,169)
+        # postion = (400, 250)
+        # pygame.draw.circle(screen, color, postion, 55)
        
-        #Circle 8
-        color = (169,169,169)
-        postion = (250, 100)
-        pygame.draw.circle(screen, color, postion, 55)
-        #Circle 9
-        color = (169,169,169)
-        postion = (250, 400)
-        pygame.draw.circle(screen, color, postion, 55)
-        pygame.display.flip()
+        # #Circle 8
+        # color = (169,169,169)
+        # postion = (250, 100)
+        # pygame.draw.circle(screen, color, postion, 55)
+        # #Circle 9
+        # color = (169,169,169)
+        # postion = (250, 400)
+        # pygame.draw.circle(screen, color, postion, 55)
+        # pygame.display.flip()
         
         
         
