fd3dc27f9917df4f799826319541f4bd134e8f34
meganclapinski25 <142531799+meganclapinski25@users.noreply.github.com>
2024-11-07 18:46:12 -0800

    Challenge 1 completed

--- game.py
+++ game.py
@@ -14,11 +14,43 @@ while running:
                 running = False
                 
         # Clear the screen 
-        screen.fill((255,255,255))
+        # screen.fill((255,255,255))
         
-        #Circle
-        color = (255, 0, 255)
-        positon = (250,250)
-        pygame.draw.circle(screen, color, positon, 75)
-        # Update
+        # #Circle
+        # color = (25, 184, 22)
+        # positon = (250,250)
+        # pygame.draw.circle(screen, color, positon, 75)
+        # # Update
+        # pygame.display.flip()
+        
+        
+        # Challenge 1 
+        screen.fill((63,63,63))
+        
+        # Circle 1 
+        color = (0,255,0)
+        postion = (100, 400)
+        pygame.draw.circle(screen, color, postion, 55)
         pygame.display.flip()
+        #Circle 2 
+        color = (255,0,0)
+        postion = (100, 100)
+        pygame.draw.circle(screen, color, postion, 55)
+        pygame.display.flip()
+        #Circle 3
+        color = (225,225,0)
+        postion = (250, 250)
+        pygame.draw.circle(screen, color, postion, 55)
+        pygame.display.flip()
+        #Circle 4
+        color = (250,141,7)
+        postion = (400, 100)
+        pygame.draw.circle(screen, color, postion, 55)
+        pygame.display.flip()
+        #Circle 5
+        color = (0,225,225)
+        postion = (400, 400)
+        pygame.draw.circle(screen, color, postion, 55)
+        pygame.display.flip()
+        
+        
