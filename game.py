f5ebc56872b0baa30e01958745174f0eccb6871d
meganclapinski25 <142531799+meganclapinski25@users.noreply.github.com>
2024-11-07 18:53:44 -0800

    Starting Challenge 2

--- game.py
+++ game.py
@@ -27,30 +27,79 @@ while running:
         # Challenge 1 
         screen.fill((63,63,63))
         
+        # # Circle 1 
+        # color = (0,255,0)
+        # postion = (100, 400)
+        # pygame.draw.circle(screen, color, postion, 55)
+        # pygame.display.flip()
+        # #Circle 2 
+        # color = (255,0,0)
+        # postion = (100, 100)
+        # pygame.draw.circle(screen, color, postion, 55)
+        # pygame.display.flip()
+        # #Circle 3
+        # color = (225,225,0)
+        # postion = (250, 250)
+        # pygame.draw.circle(screen, color, postion, 55)
+        # pygame.display.flip()
+        # #Circle 4
+        # color = (250,141,7)
+        # postion = (400, 100)
+        # pygame.draw.circle(screen, color, postion, 55)
+        # pygame.display.flip()
+        # #Circle 5
+        # color = (0,225,225)
+        # postion = (400, 400)
+        # pygame.draw.circle(screen, color, postion, 55)
+        # pygame.display.flip()
+        
+        #Challenge 2 
+        
         # Circle 1 
-        color = (0,255,0)
+        color = (169,169,169)
         postion = (100, 400)
         pygame.draw.circle(screen, color, postion, 55)
-        pygame.display.flip()
+       
         #Circle 2 
-        color = (255,0,0)
+        color = (169,169,169)
         postion = (100, 100)
         pygame.draw.circle(screen, color, postion, 55)
-        pygame.display.flip()
+        
         #Circle 3
-        color = (225,225,0)
+        color = (169,169,169)
         postion = (250, 250)
         pygame.draw.circle(screen, color, postion, 55)
-        pygame.display.flip()
+        
         #Circle 4
-        color = (250,141,7)
+        color = (169,169,169)
         postion = (400, 100)
         pygame.draw.circle(screen, color, postion, 55)
-        pygame.display.flip()
+        
         #Circle 5
-        color = (0,225,225)
+        color = (169,169,169)
         postion = (400, 400)
         pygame.draw.circle(screen, color, postion, 55)
+       
+        # Circle 6 
+        color = (169,169,169)
+        postion = (50, 50)
+        pygame.draw.circle(screen, color, postion, 55)
+       
+        #Circle 7
+        color = (169,169,169)
+        postion = (50, 50)
+        pygame.draw.circle(screen, color, postion, 55)
+       
+        #Circle 8
+        color = (169,169,169)
+        postion = (75, 75)
+        pygame.draw.circle(screen, color, postion, 55)
+        #Circle 9
+        color = (169,169,169)
+        postion = (50, 50)
+        pygame.draw.circle(screen, color, postion, 55)
         pygame.display.flip()
+        
+        
         
         
